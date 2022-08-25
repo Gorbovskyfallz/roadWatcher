f78c1148c6ed1fe76ffca9ce44d45dd3a71bd916
passed3 <funkydrumma@yandex.ru>
2022-08-25 13:24:18 +0500

    implemented mount flash func from unix lib

--- main/mountcheck.go
+++ main/mountcheck.go
@@ -1,19 +1,30 @@
 package main
 
 import (
-	"fmt"
-	minfo "github.com/moby/sys/mountinfo"
 	"golang.org/x/sys/unix"
 	"log"
 )
 
 func MountI(mountpath string) {
-	infoMount, _ := minfo.Mounted(mountpath) // а есть ли маунт в эту директорию?
-	fmt.Println(infoMount)
+	//infoMount, mountErr := minfo.Mounted(mountpath) // а есть ли маунт в эту директорию?
+	//if mountErr != nil {
+	//	log.Println("dir ", mountpath, "have no mountpoints: ", mountErr)
+	//}
+	//fmt.Println(infoMount)
+	//
+	//kek := unix.Unmount("/run/media/jupyter/E264-9720", 0)
+	//if kek != nil {
+	//	log.Panic(kek)
+	//}
 
-	kek := unix.Unmount("/run/media/jupyter/E264-9720", 0)
-	if kek != nil {
-		log.Panic(kek)
+	if mountErr := unix.Mount("/dev/sdb1", "/media/passed3", "exfat", 0, ""); mountErr != nil {
+		log.Println("error:", mountErr)
+		if mountErr.Error() == "no such file or diectory" {
+			log.Println("passed assert err")
+		}
+
+	} else {
+		log.Println("mounted")
 	}
 
 }
