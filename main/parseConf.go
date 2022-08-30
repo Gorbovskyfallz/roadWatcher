7627c66d5b4659a944457c272fab7d05e32cf17f
Gorbovslyfallz <funkydrumma@yandex.ru>
2022-08-30 22:12:43 +0500

    created structs to parse config

--- main/parseConf.go
+++ main/parseConf.go
@@ -1,1 +1,34 @@
 package main
+
+type Config struct {
+	VpnSettings       VpnSettings
+	GlobalNetSettings GlobalNetSettings
+	Security          Security
+	Flash             Flash
+	Hardware          Hardware
+}
+
+type VpnSettings struct {
+	PrivateNetwork      string
+	PingTimesForVpn     int
+	RebootOnPrivateFail bool
+	VpnRebootTimeout    int
+}
+
+type GlobalNetSettings struct {
+	GlobalNetwork       string
+	GlobalNetWorkPort   int
+	GlobalRebootTimeout int
+}
+type Security struct {
+	EnableTokenConfigParse bool
+	TokenBotApi            string
+}
+type Flash struct {
+	PathToDev      string
+	MountPointPath string
+}
+
+type Hardware struct {
+	Ledindication string
+}
