283f9a4896c438a86d9aa6aaf22899c687249f81
passed3 <funkydrumma@yandex.ru>
2022-09-02 19:24:53 +0500

    started to smoke fsnotify

--- cmd/main.go
+++ cmd/main.go
@@ -1,19 +1,26 @@
 package main
 
 import (
-	"fmt"
+	"github.com/fsnotify/fsnotify"
 	"kek/logger"
-	"kek/parseConf"
+	"log"
 )
 
 func main() {
 	// 1. считываем конфиг, возможно даже мы должны делать это в отдельной горутине для того, чтобы конфиг считывался
 	// конкурентно и подтятигивал новые параметры, которые могли бы быть измененены. Для начала просто считаем конфиг
 	// и запихнем его в объект конфига
-	logger.CreateLogFile("rpiRegLog.txt")
-	mainConf := new(parseConf.Config)
-	mainConf.ParseFromTwoDirs("regConfig.yaml", "/etc/regConfig.yaml")
-	mainConf.SwitchTokenInput()
-	fmt.Println(mainConf.Security.TokenBotApi)
+	logger.CreateLogFile("TESTFSNOTIFY.TXT")
+	watch, err := fsnotify.NewWatcher()
+	if err != nil {
+		log.Println("cant create watcher")
+	}
+	defer watch.Close()
+	err = watch.Add("./regConfig.yaml")
+	if err != nil {
+		log.Fatalln("cant add file to watcher")
+	} else {
+		log.Println("succsess")
+	}
 
 }
--- go.mod
+++ go.mod
@@ -7,3 +7,5 @@ require (
 	golang.org/x/sys v0.0.0-20220829200755-d48e67d00261
 	gopkg.in/yaml.v2 v2.4.0
 )
+
+require github.com/fsnotify/fsnotify v1.5.4 // indirect
