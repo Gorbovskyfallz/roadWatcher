6772ad9e545044261a2d9af87c264c360efa0c1d
passed3 <funkydrumma@yandex.ru>
2022-09-07 01:37:51 +0500

    refactored flash.go: naming

--- flash/flash.go
+++ flash/flash.go
@@ -24,9 +24,9 @@ type FlashUse struct {
 // MountInfo "Checking - mounted or not to mountpath
 //configMountPoint (/media/passed3/flash for e.x.)"
 func (f *Flash) MountInfo(mountPoint string) (status bool, mountErr error) {
-	funcName := "MountInfo"
+	name := "MountInfo"
 	if status, mountErr = flashInfo.Mounted(mountPoint); mountErr != nil {
-		log.Printf("%s: error \"%v\" occured\n", mountErr, funcName)
+		log.Printf("%s: error \"%v\" occured\n", mountErr, name)
 		return status, mountErr
 	}
 	f.Mounted = status
@@ -35,25 +35,25 @@ func (f *Flash) MountInfo(mountPoint string) (status bool, mountErr error) {
 
 // MountFlash mounting block device dev to mountpoint with path path
 func MountFlash(dev, mountPath string) (exitStatus int) {
-	funcName := "MountFlash"
-	var flags uintptr = unix.MS_MGC_VAL
-	err := unix.Mount(dev, mountPath, "exfat", flags, "")
+	name := "MountFlash"
+	var magicFlag uintptr = unix.MS_MGC_VAL
+	err := unix.Mount(dev, mountPath, "exfat", magicFlag, "")
 	if err != nil {
 		switch {
 		case err.Error() == "no such device":
-			log.Printf("%s: device on mountPath %s\n", funcName, err)
+			log.Printf("%s: device on mountPath %s\n", name, err)
 			exitStatus = 1
 		case err.Error() == "no such file or directory":
-			log.Printf("%s: mountpath: %s\n", funcName, err)
+			log.Printf("%s: mountpath: %s\n", name, err)
 			exitStatus = 2
 		case err.Error() == "device or resource busy":
-			log.Printf("%s: device: %s\n", funcName, err)
+			log.Printf("%s: device: %s\n", name, err)
 			exitStatus = 3
 		case err.Error() == "invalid argument":
-			log.Printf("%s: arguments: %s\n", funcName, err)
+			log.Printf("%s: arguments: %s\n", name, err)
 			exitStatus = 4
 		default:
-			log.Printf("%s: %s mnt to %s\n", funcName, dev, mountPath)
+			log.Printf("%s: %s mnt to %s\n", name, dev, mountPath)
 			exitStatus = 0
 
 		}
@@ -64,11 +64,11 @@ func MountFlash(dev, mountPath string) (exitStatus int) {
 
 // unmount all flash from mediamountdir
 func (f *Flash) UmountPoint(mountPoint string) int {
-	nameOfFunc := "UmountPoint"
+	name := "UmountPoint"
 	if unmountErr := unix.Unmount(mountPoint, 0); unmountErr != nil {
-		log.Printf("%s:error \"%s\" occured\n", nameOfFunc, unmountErr)
+		log.Printf("%s:error \"%s\" occured\n", name, unmountErr)
 	} else {
-		log.Printf("%s: %s unmounted\n", nameOfFunc, mountPoint)
+		log.Printf("%s: %s unmounted\n", name, mountPoint)
 	}
 
 	return 0 /////
@@ -79,8 +79,8 @@ func (f *Flash) UmountPoint(mountPoint string) int {
 // переделать на сисколы
 func (f *FlashUse) CheckPid(processName string) bool {
 	util := "pidof"
-	withArgs := "-s"
-	out, _ := exec.Command(util, withArgs, processName).Output()
+	args := "-s"
+	out, _ := exec.Command(util, args, processName).Output()
 	if len(out) != 0 {
 		f.ProcessWork = true
 	} else {
@@ -94,20 +94,20 @@ func (f *FlashUse) CheckService(serviceName string) bool {
 	// возможно, это стоит переписать на системных
 	// вызовах без использованися exec
 	// sudo systemctl status docker.service | grep Active
-	nameOfFunc := "CheckSrvice"
+	name := "CheckSrvice"
 	grep := exec.Command("grep", "Active")
 	command := exec.Command("systemctl", "status", serviceName)
 	pipe, _ := command.StdoutPipe()
 	defer func(pipe io.ReadCloser) {
 		closePipeErr := pipe.Close()
 		if closePipeErr != nil {
-			log.Printf("%s: %w", nameOfFunc, closePipeErr)
+			log.Printf("%s: %w", name, closePipeErr)
 		}
 	}(pipe)
 	grep.Stdin = pipe
 	startErr := command.Start()
 	if startErr != nil {
-		log.Printf("%s: %s command: %w", nameOfFunc, grep, startErr)
+		log.Printf("%s: %s command: %w", name, grep, startErr)
 	}
 	res, _ := grep.Output()
 	if strings.Contains(string(res), "active (running)") {
