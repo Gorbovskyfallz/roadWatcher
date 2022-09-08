5689974c116aa4c11671f5e78de1b35445f0c556
passed3 <funkydrumma@yandex.ru>
2022-09-08 18:24:07 +0500

    added: core

--- cmd/main.go
+++ cmd/main.go
@@ -1,27 +1,9 @@
 package main
 
-import (
-	"kek/Config"
-	"kek/logger"
-	"sync"
-)
+import "kek/core"
 
 func main() {
-	configPath, logName := Config.TakeConfigName()
-	logger.CreateLogFile(logName)
-	mainConf := new(Config.Config)
-	mainConf.ParseFromYaml(configPath)
-	wg := sync.WaitGroup{}
-	wg.Add(2)
 
-	watcher := mainConf.AddNotifyWatcher(configPath)
-	defer watcher.Close()
-
-	go func() {
-		defer wg.Done()
-		mainConf.CheckUpdate(watcher, configPath)
-	}()
-
-	wg.Wait()
+	core.Core()
 
 }
---
+++ core/core.go
@@ -0,0 +1,26 @@
+package core
+
+import (
+	"kek/Config"
+	"kek/logger"
+	"sync"
+)
+
+func Core() {
+	configPath, logName := Config.TakeConfigName()
+	logger.CreateLogFile(logName)
+	mainConf := new(Config.Config)
+	mainConf.ParseFromYaml(configPath)
+	wg := sync.WaitGroup{}
+	wg.Add(2)
+
+	watcher := mainConf.AddNotifyWatcher(configPath)
+	defer watcher.Close()
+
+	go func() {
+		defer wg.Done()
+		mainConf.CheckUpdate(watcher, configPath)
+	}()
+
+	wg.Wait()
+}
